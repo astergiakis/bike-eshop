192f3e6c1d632a36ccbcd7158f52006775fd974a
aPo <apo.kouas@gmail.com>
2021-07-29 12:02:17 +0300

    created new project with create react app and added files for homepage

---
+++ src/homepage.component.js
@@ -0,0 +1,14 @@
+import react from 'react';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'>HATS</h1>
+                <span className='subtitle'>Shop Now</span>
+            </div>
+        </div>
+        </div>
+     </div>
+)
