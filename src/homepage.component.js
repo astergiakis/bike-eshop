ff2108f5ff40b71a46ed13fad2fc328386c61127
aPo <apo.kouas@gmail.com>
2021-07-29 12:14:46 +0300

    created new project with create react app and added files for homepage

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -1,14 +1,52 @@
 import react from 'react';
 
-const Homepage = () => (
+const HomePage = () => (
     <div className='homepage'>
         <div className='directory-menu'>
         <div className='menu-item'>
             <div className='content'>
-                <h1 className='title'>HATS</h1>
-                <span className='subtitle'>Shop Now</span>
+                <h1 className='title'>BMX</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
+            </div>
+            <div className='content'>
+                <h1 className='title'>CITY</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
+            </div>
+            <div className='content'>
+                <h1 className='title'>ΠΑΙΔΙΚΑ</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
+            </div>
+            <div className='content'>
+                <h1 className='title'> MOUNTAIN</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
+            </div>
+            <div className='content'>
+                <h1 className='title'>ΠΑΤΙΝΙΑ</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
+            </div>
+            <div className='content'>
+                <h1 className='title'>ΠΑΤΙΝΙΑ</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
+            </div>
+            <div className='content'>
+                <h1 className='title'>ROAD</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
+            </div>
+            <div className='content'>
+                <h1 className='title'>ΗΛΕΚΤΡΙΚΑ ΠΟΔΗΛΑΤΑ</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
+            </div>
+            <div className='content'>
+                <h1 className='title'>ΑΝΤΑΛΛΑΚΤΙΚΑ</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
+            </div>
+            <div className='content'>
+                <h1 className='title'>ΑΞΕΣΟΥΑΡ</h1>
+                <span className='subtitle'>ΔΕΣ ΠΕΡΙΣΣΟΤΕΡΑ</span>
             </div>
         </div>
         </div>
      </div>
 )
+
+export default HomePage; 
